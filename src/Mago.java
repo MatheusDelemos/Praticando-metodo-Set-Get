738d8a46a70ae1693ca5d7c019be210310d1414b
Matheus de Lemos <matheus.delemos2507@gmail.com>
2023-03-23 22:37:50 -0300

    Segundo commit corigindo detalhes

---
+++ src/Mago.java
@@ -0,0 +1,40 @@
+//Todo construtor é metodo mas nem todo metodo é um construtor
+public class Mago {
+    //definindo as irfomaçôes do mago.
+    private int danobasico;    //O mago da um dano na forma de variável inteira
+    private String nomeMago;  // E o nome do mago é uma string/texto.
+
+   //Criando um metodo construtor para setar o nome ao criar um novo mago
+    public Mago(String nome) {
+        this.nomeMago = nome;
+    }
+
+    public boolean fireball(int ptoInteligencia, int vidaChef) {
+        //Acima o fireball esta e não o mago tem pto inteligencia e a vidaChef
+        int danototal = danobasico * ptoInteligencia;
+        //acima danototal esta recebendo danobasic multiplicando ponto inteligencia
+        boolean hitkill = vidaChef < (danobasico * ptoInteligencia);
+        //boolean aqui serve csetar um true no hitkill que é a vida do chef e o dano vezes pt inteligencia
+        System.out.println("Fireball do Mago infringiu " + danototal + " de dano");
+        return hitkill;
+        //retornando o hitkill
+    }
+
+    //Aqui setamos o dano basico  da classe mago
+    public void setDanobasico(int danobasico) {
+        this.danobasico = danobasico;
+    }
+
+    public int getDanobasico() {
+        return this.danobasico;
+    }
+//Aqui setaremos o nome do mago
+
+    public void setNomeMago(String nomeMago) {
+        this.nomeMago = nomeMago;
+    }
+
+    public String getNomeMago() {
+        return this.nomeMago;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    //Transformar nome dos magos de variavel para uma propriedado dos magos semelhante ao dano basico
+    public static void main(String[] args) {
+        int vidaChef = 260;
+        //Definindo um obejo mago arcano e setando um nome para ele
+        Mago arcano = new Mago("jorge");
+        //definindo um dano para o mago arcano
+        arcano.setDanobasico(20);
+
+        Guerreiro furia = new Guerreiro("Miguel");
+        furia.setDanoBasico(30);
+
+
+        if(furia.marretada(10,vidaChef)){
+            System.out.println("O boss foi de arrasta pra cima ");
+
+        }else{
+            System.out.println("O boss sobreviveu !");
+        }
+
+        if(arcano.fireball(10,vidaChef)){
+            System.out.println("O boss foi de comes e bebes");
+
+        }else {
+            System.out.println("O boss continua de pé ");
+        }
+
+    }
+
+}
